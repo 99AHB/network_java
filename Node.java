67cb410b65932aa611216975920437968d9d679f
99AHB <122578987+99AHB@users.noreply.github.com>
2023-02-09 13:41:12 +0900

    13

---
+++ Node.java
@@ -0,0 +1,10 @@
+public class Node {
+    public String data;
+    public Node ling;
+
+    @Override
+    public String toString() {
+        return "노드의 데이터 값은 " + data + "입니다";
+    }
+}
+
---
+++ day16.java
@@ -0,0 +1,7 @@
+public class day16 {
+    public static void main(String[] args) {
+        Node node = new Node();
+        node.data = "피카츄";
+        System.out.println(node.toString());
+    }
+}
