c23d5dd8c38d0616a81a1d8b210edd856ef156d2
99AHB <122578987+99AHB@users.noreply.github.com>
2023-02-10 17:56:20 +0900

    pikachu

--- game/Pikachu.java
+++ game/Pikachu.java
@@ -1,4 +1,19 @@
 package game;
 
 public class Pikachu {
+    private int count = 0;
+    private String owner = null;
+    private String skills = null;
+
+    Pikachu(String owner,String skills){
+        owner = o;
+        skills = s;
+    }
+    public void setOwner(String owner){
+        owner = owner;
+    }
+    public void setSkills(String skills){
+        skills = skills;
+    }
+
 }
