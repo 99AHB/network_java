85a0115e69c062decaf51ceab571a279c7ab4ab0
99AHB <122578987+99AHB@users.noreply.github.com>
2023-02-10 17:10:14 +0900

    틀잡기

--- game/pokemon.java
+++ game/pokemon.java
@@ -10,11 +10,21 @@ public class pokemon {
             System.out.println("총 마리의 포켓몬이 생성되었습니다");
             System.out.print("1) 포켓몬 생성  2) 프로그램 종료 : ");
             String menu = sc.nextLine();
+            String pokemons = sc.nextLine();
             if (menu.equals("2")) {
                 System.out.println("프로그램을 종료합니다");
                 break;
             } else if (menu.equals("1")) {
-
+                System.out.println("1) 피카츄 2) 꼬부기 3) 파이리 :");
+                if(pokemons.equals("1")){
+                    //피카츄 클레스 호출
+                }
+                else if(pokemons.equals("2")){
+                    //꼬부기 클레스 호출
+                }
+                else if(pokemons.equals("3")){
+                    //파이리 클레스 호출
+                }
             } else {
                 System.out.println("메뉴에서 골라 주세요");
             }
